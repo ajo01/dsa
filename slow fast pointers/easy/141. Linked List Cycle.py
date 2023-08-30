1ecdfe7c8ea1f84dc9f2201010543657a7196e12
Amy Jo <amyjojjm2001@gmail.com>
2023-08-29 23:10:28 -0700

    day 6-3

---
+++ slow fast pointers/easy/141. Linked List Cycle.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        turtle = head
+        rabbit = head
+
+        while rabbit and rabbit.next:
+            turtle = turtle.next
+            rabbit = rabbit.next.next
+
+            if turtle == rabbit:
+                return True
+        return False
+           
+        
