1467b15c4d704f5abdeeb6a41c699c77ba81def6
Amy Jo <amyjojjm2001@gmail.com>
2023-09-17 23:05:46 -0700

    day 25

---
+++ dict/medium/567. Permutation in String.py
@@ -0,0 +1,59 @@
+# dict string
+# not optimized
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+       
+        d1 = {}
+
+        n = len(s1)
+        n2 = len(s2)
+
+        if n > n2:
+            return False
+
+        for i in range(n):
+            if s1[i] in d1:
+                d1[s1[i]] += 1
+            else:
+                d1[s1[i]] = 1
+
+        for i in range(n2 - n + 1):
+            d2 = {}
+            for j in range(i, i+n):
+                if s2[j] in d2:
+                    d2[s2[j]] += 1
+                else:
+                    d2[s2[j]] = 1
+            flag = True
+            for k in d1:
+                if k not in d2 or d1[k] != d2[k]:
+                    flag = False
+            if flag is True:
+                return True
+        return False
+
+
+# use counter to optimize
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n = len(s1)
+        n2 = len(s2)
+
+        if n > n2:
+            return False
+
+        counter = Counter(s1)
+
+        for i in range(n2 - n + 1):
+            tmp = s2[i:i+n]
+            counter2 = Counter(tmp)
+            print(counter, counter2)
+            flag = True
+            for k in counter:
+                if k not in counter2 or counter[k] != counter2[k]:
+                    flag = False
+            if flag is True:
+                return True
+        return False
