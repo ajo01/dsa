204698083c6fb4bb6c1a6ca3f9b7c93b8eee9dea
Amy Jo <amyjojjm2001@gmail.com>
2023-09-20 02:16:46 -0700

    two sum more optimize

--- arrays/easy/1. Two Sum.py
+++ arrays/easy/1. Two Sum.py
@@ -1,6 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         List[int] 
+        d = {}
+        for i in range(0, len(nums) ):
+            n = target-nums[i]
+            if n in d:
+                return [i, d[n]]
+            d[nums[i]] = i
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        List[int] 
+        map = {}
+
+        for i in range(0, len(nums)):
+            map[nums[i]] = i
+
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+            if complement in map and map[complement] !=i:
+                return [i, map[complement]]
+            
+        return []
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        List[int] 
         for i in range(0, len(nums)):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
