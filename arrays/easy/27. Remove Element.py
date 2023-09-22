e1b5ee87c563df9f93614c56d90c760083ac2e23
Amy Jo <amyjojjm2001@gmail.com>
2023-09-22 10:12:12 -0700

    day30

---
+++ arrays/easy/27. Remove Element.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index+=1
+        return index
+
+# [3, 2, 2, 3] k =3 -> [3] [2] [2, 2] [2, 2, 3]
