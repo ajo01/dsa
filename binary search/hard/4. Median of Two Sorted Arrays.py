0d4b8572dcb2e8c4eae2933ffd9d1ea77bdbe35d
Amy Jo <amyjojjm2001@gmail.com>
2023-09-24 14:05:40 -0700

    dat 32

--- binary search/hard/4. Median of Two Sorted Arrays.py
+++ binary search/hard/4. Median of Two Sorted Arrays.py
@@ -1,6 +1,51 @@
+# O(n+m)log(n+m)  space O(1)
+# binary search
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1 = len(nums1)
+        n2 = len(nums2)
+
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        lo = 0
+        hi = n1
+        n = n1 + n2
+        left = (n + 1) // 2
+
+
+        while lo <= hi:
+            mid1 = (lo + hi)//2
+            mid2 = left - mid1
+
+            l1 = float('-inf')
+            l2 = float('-inf')
+            r1 = float('-inf')
+            r2 = float('-inf')
+
+            if mid1 < n1:
+                r1 = nums1[mid1]
+            if mid2 < n2:
+                r2 = nums2[mid2]
+            if mid1 - 1 >= 0:
+                l1 = nums1[mid1 - 1]
+            if mid2 - 1 >= 0:
+                l2 = nums2[mid2 -1]
+
+            if l1 <= r2 and l2 <= r1: 
+                if n % 2 == 1:
+                    return max(l1, l2)
+                else:
+                    return (max(l1, l2) + min(r1, r2))/2.0
+            elif l1 > r2: 
+                hi = mid1 - 1
+            else: 
+                lo = mid1 + 1
+        return 0
+        
 
 # two pointers
-# O(n+m)
+# time O(n+m)  space O(1)
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         n = len(nums1)
@@ -35,7 +80,7 @@ class Solution:
 
 
 # brute force
-# O(n+m)log(n+m)
+# time O(n+m) space O(n+m)
 
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
