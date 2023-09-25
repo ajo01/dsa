9a5e6c270dd2a9a8a38dabdfa2f4ccdeb3df3329
Amy Jo <amyjojjm2001@gmail.com>
2023-09-25 01:22:01 -0700

    optimal sol

--- sliding window/medium/3. Longest Substring Without Repeating Characters.py
+++ sliding window/medium/3. Longest Substring Without Repeating Characters.py
@@ -1,4 +1,31 @@
+# categories: arrays, strings, sliding window
+
+#optimal 
+# space O(n) n is input s
+# time O(n)  going through all of s only once
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length = 0
+        n = len(s)
+        l = 0
+        charSet = set()
+        for r in range(n):
+            if s[r] not in charSet:
+                charSet.add(s[r])
+                max_length = max(max_length, r - l + 1)
+            else:
+                while s[r] in charSet:
+                    charSet.remove(s[l])
+                    l+=1
+                charSet.add(s[r])
+        return max_length
+
+
+
+
 # not optimal
+# brute force
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
