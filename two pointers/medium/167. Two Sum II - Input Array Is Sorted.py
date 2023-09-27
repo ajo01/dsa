514b791a330a781497df3c8e6931de4ea0227e6f
Amy Jo <amyjojjm2001@gmail.com>
2023-09-26 22:51:08 -0700

    day 35-2

---
+++ two pointers/medium/167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,15 @@
+
+# O(1) space O(n) time
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            n = numbers[i] + numbers[j]
+            if n == target:
+                return [i+1, j+1]
+            if n > target:
+                j-= 1
+            else:
+                i+=1
